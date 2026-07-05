7fe500f42994a9af4e92c2f321675b6da4c48fbd
Geronimo Valentin Langenheim <langenheimgeronimovalentin@gmail.com>
2026-07-05 01:37:02 -0300

    Add ProductoService

--- src/main/java/com/example/catalogoservice/service/ProductoService.java
+++ src/main/java/com/example/catalogoservice/service/ProductoService.java
@@ -1,5 +1,26 @@
 package com.example.catalogoservice.service;
 
-public class ProductoService {
-    
+import com.example.catalogoservice.model.dto.ProductoAdminResponseDTO;
+import com.example.catalogoservice.model.dto.ProductoCreateDTO;
+import com.example.catalogoservice.model.dto.ProductoResponseDTO;
+import com.example.catalogoservice.model.dto.ProductoUpdateDTO;
+import com.example.catalogoservice.model.enums.State;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ProductoService {
+
+    ProductoResponseDTO createProducto(ProductoCreateDTO dto);
+
+    ProductoResponseDTO getProducto(Long id);
+
+    Page<ProductoResponseDTO> getProductos(String nombre, Long categoriaId, State state, Pageable pageable);
+
+    ProductoResponseDTO updateProducto(Long id, ProductoUpdateDTO dto);
+
+    void deleteProducto(Long id);
+
+    ProductoResponseDTO changeState(Long id, State newState);
+
+    ProductoAdminResponseDTO getProductoAdmin(Long id);
 }
